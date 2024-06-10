70632606d77afefc848669e5f204ff53bc7d3cde
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-06-10 22:43:22 +0300

    Adding the task list component and tasks in json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { Observable, Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
