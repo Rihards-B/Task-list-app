0da866b659872e4a72ddf1fb9ffeee389567c279
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-06-21 10:12:22 +0300

    Creating an error component and adding form validation to add task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,18 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskListComponent } from '../task-list/task-list.component';
 import { TaskService } from '../../sevices/task.service';
 import { Task } from '../../models/task';
+import { TaskFormValidationService } from 'src/app/sevices/taskFormValidation.service';
+import { title } from 'process';
+import { FormErrorComponent } from '../form-error/form-error.component';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, NgFor, TaskListComponent],
+  imports: [ReactiveFormsModule, FormsModule, NgFor, TaskListComponent, FormErrorComponent, NgClass],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
@@ -20,25 +23,31 @@ export class AddTaskComponent implements OnInit {
   ]
   @ViewChild('addformtemplate') popupContent?: TemplateRef<any>;
   formGroup!: FormGroup;
+
   constructor(private formBuilder: FormBuilder,
       private dialogRef: MatDialog,
-      private taskService: TaskService) { }
+      private taskService: TaskService,
+      private taskFormValidationService: TaskFormValidationService) { }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
-      title: ["", [Validators.required,
-        
-      ]],
+      title: ["", {validators: [Validators.required,
+        this.taskFormValidationService.uniqueTitle(),
+      ]}],
       description: [""],
-      type: [""]
+      type: ["", {validators: [Validators.required]}]
     })
   }
 
   submit() {
-    let formResult = this.formGroup.value;
-    let task: Task = new Task(formResult.title, formResult.description, formResult.type, "incomplete");
-    this.taskService.addTask(task);
-    this.dialogRef.closeAll();
+    this.formGroup.markAllAsTouched();
+    if(this.formGroup.valid) {
+      let formResult = this.formGroup.value;
+      let task: Task = new Task(formResult.title, formResult.description, formResult.type, "incomplete");
+      this.taskService.addTask(task);
+      this.dialogRef.closeAll();
+      this.formGroup.reset();
+    }
   }
 
   openForm() {
---
+++ src/app/components/form-error/form-error.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-error.component.html',
+  styleUrl: './form-error.component.scss'
+})
+export class FormErrorComponent {
+  @Input() control: AbstractControl | null = null;
+}
