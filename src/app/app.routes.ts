ce5271a380524efaa04293f079916ca0bf9c2ceb
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-07-01 16:59:29 +0300

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TasksCardComponent } from './components/tasks-card/tasks-card.component';
+import { AddTaskFormComponent } from './components/add-task-form/add-task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: TasksCardComponent},
+    {path: "add-task", component: AddTaskFormComponent}
+];
