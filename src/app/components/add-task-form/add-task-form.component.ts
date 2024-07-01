54d001f81b936afb40a56c21d849e464f3ed671d
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-07-01 16:54:56 +0300

    Separating add task form from butotn

--- src/app/components/add-task-form/add-task-form.component.ts
+++ src/app/components/add-task-form/add-task-form.component.ts
@@ -1,33 +1,34 @@
-import { NgClass, NgFor } from '@angular/common';
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { TaskListComponent } from '../task-list/task-list.component';
 import { TaskService } from '../../sevices/task.service';
 import { Task } from '../../models/task';
 import { TaskFormValidationService } from 'src/app/sevices/taskFormValidation.service';
-import { title } from 'process';
 import { FormErrorComponent } from '../form-error/form-error.component';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-add-task',
+  selector: 'app-add-task-form',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, NgFor, TaskListComponent, FormErrorComponent, NgClass],
-  templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  imports: [FormErrorComponent, ReactiveFormsModule, FormsModule, TaskListComponent, FormErrorComponent, RouterModule],
+  templateUrl: './add-task-form.component.html',
+  styleUrl: './add-task-form.component.scss'
 })
-export class AddTaskComponent implements OnInit {
+export class AddTaskFormComponent implements OnInit, OnDestroy, AfterViewInit {
   typeOptions: string[] = [
     "Story",
     "Task"
   ]
+
   @ViewChild('addformtemplate') popupContent?: TemplateRef<any>;
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
       private dialogRef: MatDialog,
       private taskService: TaskService,
-      private taskFormValidationService: TaskFormValidationService) { }
+      private taskFormValidationService: TaskFormValidationService,
+      private router: Router) { }
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
@@ -36,6 +37,18 @@ export class AddTaskComponent implements OnInit {
       ]}],
       description: [""],
       type: ["", {validators: [Validators.required]}]
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    this.dialogRef.closeAll();
+  }
+
+  ngAfterViewInit(): void {
+    this.openForm();
+    this.dialogRef.afterAllClosed.subscribe(() => {
+      this.router.navigateByUrl("/");
     })
   }
 
