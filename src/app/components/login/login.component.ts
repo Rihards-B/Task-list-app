1ff8d7054cd81cc6245fe865af229fdd221507f0
Rihards Blekus <rihardsblekus2002+01@gmail.com>
2024-09-10 11:42:53 +0300

    Creating a login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormErrorComponent } from '../form-error/form-error.component';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from 'src/app/sevices/auth.service';
+import { Router } from '@angular/router';
+import { BehaviorSubject, from, Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormErrorComponent, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  failedLoginSubject = new BehaviorSubject<boolean>(false);
+  formGroup: FormGroup = this.formBuilder.group({
+    username: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+    password: ["", [Validators.required, Validators.minLength(6), Validators.maxLength(64)]]
+  })
+
+  constructor(private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router) {}
+
+  login() {
+    this.authService.login(this.formGroup.value).subscribe((response) => {
+      if (response.isLoggedIn === true) {
+        this.router.navigateByUrl("/");
+      } else {
+        this.failedLoginSubject.next(true);
+      }
+    })
+  }
+}
