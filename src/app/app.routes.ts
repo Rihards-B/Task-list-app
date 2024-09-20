39f1f66cbfcbbe25feebdfc121210d54d3286978
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-09-20 10:38:00 +0300

    Renaming the guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { AddTaskFormComponent } from './components/add-task-form/add-task-form.c
 import { TaskDetailsComponent } from './components/task-details/task-details.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/login/login.component';
-import { canActivateLoggedIn, canActivateIsLoggedIn } from './guards/auth.guard';
+import { loggedInGuard, blockLoggedInUserGuard } from './guards/auth.guard';
 import { TaskResolver } from './resolvers/taskResolver';
 
 export const routes: Routes = [
-    { path: "", component: TasksCardComponent, canActivate: [canActivateLoggedIn] },
+    { path: "", component: TasksCardComponent, canActivate: [loggedInGuard] },
     { path: "add-task", component: AddTaskFormComponent },
     { path: ":id/details", component: TaskDetailsComponent, resolve: { task: TaskResolver } },
-    { path: "login", component: LoginComponent, canActivate: [canActivateIsLoggedIn] },
+    { path: "login", component: LoginComponent, canActivate: [blockLoggedInUserGuard] },
     { path: "404", component: PageNotFoundComponent },
     { path: "**", redirectTo: "/404" }
 ];
