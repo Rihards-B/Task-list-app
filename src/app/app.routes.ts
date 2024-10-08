ca743ddd32b6f2ea8f34772c8305328e5a731157
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-08 08:13:45 +0300

    Adding a route for user details, adding the user details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './components/login/login.component';
 import { canActivateLoggedIn, canActivateIsLoggedIn } from './guards/auth.guard';
 import { TaskResolver } from './resolvers/taskResolver';
 import { RegisterComponent } from './components/register/register.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserResolver } from './resolvers/user-resolver';
+import { RoleResolver } from './resolvers/role.resolver';
+import { isAdminGuard } from './guards/role.guard';
 
 export const routes: Routes = [
     { path: "", component: TasksCardComponent, canActivate: [canActivateLoggedIn] },
@@ -14,6 +18,7 @@ export const routes: Routes = [
     { path: ":id/details", component: TaskDetailsComponent, resolve: { task: TaskResolver } },
     { path: "login", component: LoginComponent, canActivate: [canActivateIsLoggedIn] },
     { path: "register", component: RegisterComponent, canActivate: [canActivateIsLoggedIn] },
+    { path: "users/:id", component: UserDetailsComponent, resolve: { user: UserResolver, roles: RoleResolver }, canActivate: [isAdminGuard] },
     { path: "404", component: PageNotFoundComponent },
     { path: "**", redirectTo: "/404" }
 ];
---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,85 @@
+import { Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, Subject, take } from 'rxjs';
+import { Role } from 'src/app/models/role-model';
+import { User } from 'src/app/models/user';
+import { RoleComponent } from '../role/role.component';
+import { RemoveButtonComponent } from 'src/app/remove-button/remove-button.component';
+import { UserService } from 'src/app/sevices/user.service';
+
+@Component({
+  selector: 'app-user-details',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule, RoleComponent, RemoveButtonComponent],
+  templateUrl: './user-details.component.html',
+  styleUrl: './user-details.component.scss'
+})
+export class UserDetailsComponent implements OnInit {
+  user: User | undefined = undefined;
+  roles: Role[] | undefined = undefined;
+  unusedRoles: Role[] | undefined = undefined;
+  userRoles: Subject<Role[]> = new BehaviorSubject<Role[]>([]);
+  userFormGroup: FormGroup = this.formBuilder.group({
+    first_name: [""],
+    last_name: [""],
+    username: [""],
+    roles: [""]
+  });
+  addRoleFormGroup: FormGroup = this.formBuilder.group({
+    add_role: [""]
+  });
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.data.pipe(take(1)).subscribe(({ user, roles }) => {
+      this.user = user;
+      console.log(this.user);
+      this.roles = roles;
+      this.updateUnusedRoles();
+      if (this.user) {
+        this.userFormGroup.patchValue(this.user);
+      }
+    })
+  }
+
+  addRole() {
+    console.log(this.userFormGroup);
+    const selectedRoleId: string = this.addRoleFormGroup.value["add_role"];
+    const roleToAdd = this.roles?.find(role => role._id === selectedRoleId);
+    if (roleToAdd && this.user) {
+      this.userFormGroup.controls["roles"].value.push(roleToAdd);
+      this.updateUnusedRoles();
+    }
+  }
+
+  removeRole(id: string) {
+    if (this.user) {
+      const roleToRemove = this.user.roles.find(role => role._id === id);
+      if (roleToRemove) {
+        this.user.roles.splice(this.user.roles.indexOf(roleToRemove), 1);
+        console.log(roleToRemove);
+        this.updateUnusedRoles();
+      }
+    }
+  }
+
+  updateUnusedRoles() {
+    // Filtering out roles the user already has and also the admin role
+    this.unusedRoles = this.roles?.filter(role =>
+      !(this.user?.roles.some(userRole => userRole._id === role._id)) &&
+      role.role_name !== "Admin");
+  }
+
+  updateUser() {
+    if (this.user && this.user._id) {
+      this.userService.updateUser(this.userFormGroup.value, this.user._id).subscribe(() => {
+        this.router.navigateByUrl('/');
+      });
+    }
+  }
+}
