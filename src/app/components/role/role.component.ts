354b550e6722a4764da434e9c5e5bbb5b686361c
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-08 08:11:30 +0300

    Adding a role component

---
+++ src/app/components/role/role.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Role } from 'src/app/models/role-model';
+import { RemoveButtonComponent } from 'src/app/remove-button/remove-button.component';
+
+@Component({
+  selector: 'app-role',
+  standalone: true,
+  imports: [RemoveButtonComponent],
+  templateUrl: './role.component.html',
+  styleUrl: './role.component.scss'
+})
+export class RoleComponent {
+  @Input() role?: Role | null = null;
+}
