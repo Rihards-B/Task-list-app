566be93ebbb6026a514a756f09bc26f24905ecfe
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-08 09:10:05 +0300

    Adding a confirm password field

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormErrorComponent } from '../form-error/form-error.component';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { backend_auth } from 'src/app/constants/endpoints';
 import { BehaviorSubject, Subject, tap } from 'rxjs';
 import { AuthService } from 'src/app/sevices/auth.service';
 import { Router } from '@angular/router';
+import { PasswordsMatch } from 'src/app/validators/matching.validator';
 
 @Component({
   selector: 'app-register',
@@ -20,6 +19,7 @@ export class RegisterComponent {
   formGroup: FormGroup = this.formBuilder.group({
     username: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
     password: ["", [Validators.required, Validators.minLength(6), Validators.maxLength(64)]],
+    confirm_password: ["", [Validators.required, Validators.minLength(6), Validators.maxLength(64), PasswordsMatch("password")]],
     first_name: ["", [Validators.required, Validators.maxLength(64)]],
     last_name: ["", [Validators.required, Validators.maxLength(64)]]
   })
