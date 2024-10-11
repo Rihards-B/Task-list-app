3531783453a4a73906b3f1dd5a06eac5da690d42
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-11 12:14:05 +0300

    Adding ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch(), withInterceptors([AuthInterceptor]))]
+  providers: [provideRouter(routes),
+  provideClientHydration(),
+  provideAnimationsAsync(),
+  provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
+  importProvidersFrom([TranslateModule.forRoot({
+    loader: {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    }
+  })])
+  ]
 };
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
