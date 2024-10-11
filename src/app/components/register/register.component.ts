17dad45faa7515cafeee6da3d7a283503c4d5bd2
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-11 12:37:00 +0300

    Adding unsubscribe to register backend call on destroy

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormErrorComponent } from '../form-error/form-error.component';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject, Subject, tap } from 'rxjs';
+import { BehaviorSubject, Subject, Subscription, tap } from 'rxjs';
 import { AuthService } from 'src/app/sevices/auth.service';
 import { Router } from '@angular/router';
 import { PasswordsMatch } from 'src/app/validators/matching.validator';
@@ -16,7 +16,8 @@ import { HttpErrorResponse } from '@angular/common/http';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
+  registerSubscription = Subscription.EMPTY;
   registerErrorsSubject: Subject<string[]> = new BehaviorSubject<string[]>([]);
   formGroup: FormGroup = this.formBuilder.group({
     username: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
@@ -28,9 +29,13 @@ export class RegisterComponent {
 
   constructor(private formBuilder: FormBuilder, private authService: AuthService, private userService: UserService, private router: Router) {}
 
+  ngOnDestroy(): void {
+    this.registerSubscription.unsubscribe();
+  }
+
   register() {
     if (this.formGroup.valid) {
-      this.authService.register(this.formGroup.value, this.registerErrorsSubject).pipe(
+      this.registerSubscription = this.authService.register(this.formGroup.value, this.registerErrorsSubject).pipe(
         tap({
           next: (response) => {
             if (response.isLoggedIn) {
