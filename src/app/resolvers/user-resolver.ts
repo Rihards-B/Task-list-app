1e985ba6b895cc9660e4bb893ac596635f695e02
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-24 12:49:11 +0300

    Changing from or to nullish coalescing

--- src/app/resolvers/user-resolver.ts
+++ src/app/resolvers/user-resolver.ts
@@ -6,7 +6,7 @@ import { UserService } from "../sevices/user.service";
 export const UserResolver: ResolveFn<User> = (
     route: ActivatedRouteSnapshot,
 ) => {
-    const id: string = route.paramMap.get('id') || ' ';
+    const id: string = route.paramMap.get('id') ?? ' ';
     console.log("Getting user with ID:", id);
     const userService = inject(UserService);
     return userService.getUser(id);
