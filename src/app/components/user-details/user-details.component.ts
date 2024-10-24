f473802803bd48a0de39968e833e8ab4507bfbd6
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-10-24 12:56:11 +0300

    Moving variable initialization in user-details out of onInit

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject, Subject, take } from 'rxjs';
@@ -16,9 +16,9 @@ import { UserService } from 'src/app/sevices/user.service';
   styleUrl: './user-details.component.scss'
 })
 export class UserDetailsComponent implements OnInit {
-  user: User | undefined = undefined;
-  roles: Role[] | undefined = undefined;
-  unusedRoles: Role[] | undefined = undefined;
+  user: User = this.activatedRoute.snapshot.data["user"];
+  roles: Role[] = this.activatedRoute.snapshot.data["roles"];
+  unusedRoles: Role[] = [];
   userRoles: Subject<Role[]> = new BehaviorSubject<Role[]>([]);
   userIsAdmin: boolean = false;
   userFormGroup: FormGroup = this.formBuilder.group({
@@ -37,34 +37,30 @@ export class UserDetailsComponent implements OnInit {
     private router: Router) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.pipe(take(1)).subscribe(({ user, roles }) => {
-      this.user = user;
-      this.roles = roles;
-      this.updateUnusedRoles();
-      if (this.user?.roles.find(role => role.roleName === "Admin")) {
-        this.userIsAdmin = true;
-      } else {
-        this.userFormGroup.disable();
-      }
-      if (this.user) {
-        this.userFormGroup.patchValue(this.user);
-      }
-    })
-  }
-
-  addRole() {
-    console.log(this.userFormGroup);
-    const selectedRoleId: string = this.addRoleFormGroup.value["addRole"];
-    const roleToAdd = this.roles?.find(role => role._id === selectedRoleId);
-    if (roleToAdd && this.user) {
-      this.userFormGroup.controls["roles"].value.push(roleToAdd);
-      this.updateUnusedRoles();
+    this.updateUnusedRoles();
+    if (this.user?.roles.find(role => role.roleName === "Admin")) {
+      this.userIsAdmin = true;
+    } else {
+      this.userFormGroup.disable();
+    }
+    if (this.user) {
+      this.userFormGroup.patchValue(this.user);
     }
   }
 
-  removeRole(id: string) {
+  addRole() {
+    const selectedRoleName: string = this.addRoleFormGroup.value["addRole"];
+    const roleToAdd = this.roles?.find(role => role.roleName === selectedRoleName);
+    if (roleToAdd && this.user) {
+      this.userFormGroup.controls["roles"].value.push(roleToAdd);
+      this.updateUnusedRoles();
+      this.addRoleFormGroup.reset();
+    }
+  }
+
+  removeRole(name: string) {
     if (this.user) {
-      const roleToRemove = this.user.roles.find(role => role._id === id);
+      const roleToRemove = this.user.roles.find(role => role.roleName === name);
       if (roleToRemove) {
         this.user.roles.splice(this.user.roles.indexOf(roleToRemove), 1);
         this.updateUnusedRoles();
@@ -75,7 +71,7 @@ export class UserDetailsComponent implements OnInit {
   updateUnusedRoles() {
     // Filtering out roles the user already has and also the admin role
     this.unusedRoles = this.roles?.filter(role =>
-      !(this.user?.roles.some(userRole => userRole._id === role._id)) &&
+      !(this.user?.roles.some(userRole => userRole.roleName === role.roleName)) &&
       role.roleName !== "Admin");
   }
 
