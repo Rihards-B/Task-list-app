3dc567321de5888cc2d56925c2c407234a90c9ad
Rihards-Blekus <rihards.blekus@vasscompany.com>
2024-12-03 14:35:44 +0200

    Adding an errors array to AuthStore state

--- src/app/store/auth/auth.state.ts
+++ src/app/store/auth/auth.state.ts
@@ -3,9 +3,11 @@ import { User } from "src/app/models/user.model";
 export interface AuthStateInterface {
     isLoggedIn: boolean | null;
     currentUser: User | null;
+    errors: string[];
 }
 
 export const InitialAuthState: AuthStateInterface = {
     isLoggedIn: null,
-    currentUser: null
+    currentUser: null,
+    errors: []
 }
