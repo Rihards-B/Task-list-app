d6ceba921cd8e4d3240c7dce02b017fd8018b98b
Rihards-Blekus <rihards.blekus@vasscompany.com>
2025-02-11 12:44:12 +0200

    Removing a left over if statement

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -4,7 +4,6 @@ import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from "@ang
 import { RoleService } from "../sevices/role.service";
 import { Observable, of, map, tap, filter } from "rxjs";
 import { AuthStore } from "../store/auth/auth.store";
-import { User } from "../models/user.model";
 import { toObservable } from "@angular/core/rxjs-interop";
 
 export const isAdminGuard: CanActivateFn = (
@@ -20,10 +19,7 @@ export const isAdminGuard: CanActivateFn = (
         return currentUser$.pipe(
             filter(user => user != null),
             map(user => {
-                if (user) {
-                    return roleService.isAdmin(user);
-                }
-                return false;
+                return roleService.isAdmin(user);
             })
         )
     } else {
